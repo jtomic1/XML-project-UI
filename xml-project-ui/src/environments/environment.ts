e6d973e74c59c63d48dc25298936dacd2dfa588f
Djordje Jovanovic <djordjejovanovic27@gmail.com>
2023-02-05 23:05:53 +0100

    New paths

--- xml-project-ui/src/environments/environment.ts
+++ xml-project-ui/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   baseUrl: 'http://localhost:8070/api',
   aServiceUrl: 'http://localhost:8080/api/a1',
-  patentUrl: 'http://localhost:8080/api/patent',
+  patentUrl: 'http://localhost:8090/api/patent',
   zigServiceUrl: 'http://localhost:8082/api/zig',
 };
 
